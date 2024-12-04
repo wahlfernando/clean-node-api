2a976c23ec734bc43a19822e1e07f21b9380c6a7
Fernando Alexandre Wahl <fernando.wahl@tbdc.com.br>
2024-12-04 11:35:53 -0300

    teste de verificação para password e passwordConfirmatrion

--- src/presentation/controllers/singup.spec.ts
+++ src/presentation/controllers/singup.spec.ts
@@ -1,33 +1,63 @@
-import test, { describe } from "node:test"
-import { SingUpController } from "./singup"
-import { MissisgParamError } from "../erros/missing-parans-error"
+import test, { describe } from "node:test";
+import { SingUpController } from "./singup";
+import { MissisgParamError } from "../erros/missing-parans-error";
 
-describe('SingUp COntroller', () => {
-  it('Shold return 400 if not name is provided', () => {
-    const sut = new SingUpController()
+describe("SingUp COntroller", () => {
+  it("Shold return 400 if not name is provided", () => {
+    const sut = new SingUpController();
     const httpRequest = {
       body: {
-        email: 'any_email@gmail.com',
-        password: 'any_password',
-        passwordConfirmatrion: 'any_password', 
-      }
-    }
-    const httpResposnse = sut.handle(httpRequest)
-    expect(httpResposnse.statusCode).toBe(400)
-    expect(httpResposnse.body).toEqual(new MissisgParamError("name"))
-  })
-})
+        email: "any_email@gmail.com",
+        password: "any_password",
+        passwordConfirmatrion: "any_password",
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new MissisgParamError("name"));
+  });
+});
 
-test('Should return 400 if no email is provided', () => {
-  const sut = new SingUpController()
-  const httpRequest = { 
+test("Should return 400 if no email is provided", () => {
+  const sut = new SingUpController();
+  const httpRequest = {
     body: {
-      name: 'any_name',
-      password: 'any_password',
-      passwordConfirmatrion: 'any_password', 
-    }
-  }
-  const httpResposnse = sut.handle(httpRequest)
-  expect(httpResposnse.statusCode).toBe(400)
-  expect(httpResposnse.body).toEqual(new MissisgParamError("email"))
-})
+      name: "any_name",
+      password: "any_password",
+      passwordConfirmatrion: "any_password",
+    },
+  };
+  const httpResponse = sut.handle(httpRequest);
+  expect(httpResponse.statusCode).toBe(400);
+  expect(httpResponse.body).toEqual(new MissisgParamError("email"));
+});
+
+test("Should return 400 if no password is provided", () => {
+  const sut = new SingUpController();
+  const httpRequest = {
+    body: {
+      name: "any_name",
+      email: "any_email@gmail.com",
+      passwordConfirmatrion: "any_password",
+    },
+  };
+  const httpResponse = sut.handle(httpRequest);
+  expect(httpResponse.statusCode).toBe(400);
+  expect(httpResponse.body).toEqual(new MissisgParamError("password"));
+});
+
+test("Should return 400 if no passwordConfirmatrion is provided", () => {
+  const sut = new SingUpController();
+  const httpRequest = {
+    body: {
+      name: "any_name",
+      email: "any_email@gmail.com",
+      password: "any_password",
+    },
+  };
+  const httpResponse = sut.handle(httpRequest);
+  expect(httpResponse.statusCode).toBe(400);
+  expect(httpResponse.body).toEqual(
+    new MissisgParamError("passwordConfirmatrion")
+  );
+});
--- src/presentation/controllers/singup.ts
+++ src/presentation/controllers/singup.ts
@@ -4,12 +4,16 @@ import { badRequest } from "../helpers/http-helper";
 
 export class SingUpController {
   handle(httpRequest: HttpRequest): HttpResposnse {
-    if (!httpRequest.body.name) {
-      return badRequest(new MissisgParamError("name"));
-    }
-
-    if (!httpRequest.body.email) {
-      return badRequest(new MissisgParamError("email"));
+    const requiredFields = [
+      "name",
+      "email",
+      "password",
+      "passwordConfirmatrion",
+    ];
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissisgParamError(field));
+      }
     }
   }
 }
